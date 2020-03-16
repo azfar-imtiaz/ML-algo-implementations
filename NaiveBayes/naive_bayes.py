3d15c2cd018bd2633ff7845168c26ad25ec27219
azfar-imtiaz <azfar.imtiaz@live.com>
2020-03-16 20:54:37 +0100

    Add comments to code for understanding

--- NaiveBayes/naive_bayes.py
+++ NaiveBayes/naive_bayes.py
@@ -6,8 +6,15 @@ class NaiveBayesClassifier:
         self._priors = None
         self._means = None
         self._vars = None
+        self._classes = None
 
     def fit(self, X, y):
+        '''
+            In the fit function of Naive Bayes, we calculate the prior probability, the mean
+            and the variance for each class separately. This defines the probabilistic nature
+            of the Naive Bayes classifier, and allows us to compute the label and probability
+            of a given instance at test time.
+        '''
         num_samples, num_features = X.shape
         self._classes = list(set(y))
         num_classes = len(self._classes)
@@ -20,6 +27,8 @@ class NaiveBayesClassifier:
         self._means = dict()
         self._vars = dict()
 
+        # for each class, we calculate the prior, mean and variance
+        # the mean and variance are calculated across all features
         for cl in self._classes:
             # first, get all records belonging to this class
             X_cls = X[cl == y]
@@ -37,6 +46,15 @@ class NaiveBayesClassifier:
             self._vars[cl] = var
 
     def predict(self, X):
+        '''
+            At test time, we predict the label of each test instance, along with its
+            probability. For a given test instance, we calculate the probability for
+            each class separately, and return the class with the highest probability.
+            This probability that we calculate at test time is the posterior probability,
+            and is calculated by using the priors we computed using the training data,
+            and calculating the likelihood of each class. This likelihood is computed
+            using the Probability Density Function.
+        '''
         y_pred = [self._predict(x) for x in X]
         return y_pred
 
@@ -44,18 +62,23 @@ class NaiveBayesClassifier:
         # posteriors = np.zeros(len(self._classes))
         posteriors = dict()
 
+        # We calculate the posterior probability of each class using the Naive Bayes formula
+        # The Naive Bayes formula is: (Likelihood x Prior) / Evidence
+        # We ignore the Evidence in Naive Bayes, so we calculate the Likelihood, and use the prior calculated above
+        # The evidence is P(x|c), and it is calculated using a PDF (Probability Density Function) for each class separately
         for cl in self._classes:
             prior = self._priors[cl]
-            
-            # compute the class conditional probability through the probability density function - gaussian, in this case
+
+            # compute the class conditional probability through the PDF - Gaussian, in this case
             mean = self._means[cl]
             var = self._vars[cl]
             numerator = np.exp(-(x - mean)**2 / (2 * var))
             denominator = np.sqrt(2 * np.pi * var)
-            cls_conditional_prob = numerator / denominator
+            likelihood = numerator / denominator
 
-            posterior = np.sum(np.log(prior + cls_conditional_prob))
+            # the log is taken here to simplify the calculations
+            posterior = np.sum(np.log(prior + likelihood))
             posteriors[cl] = posterior
 
         # return np.argmax(posteriors)
-        return  max(posteriors, key = lambda k: posteriors[k])
+        return max(posteriors, key=lambda k: posteriors[k])
